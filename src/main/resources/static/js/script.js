6e0ba11c5e7a82c838d64f6d8b108a2f670523e6
Kailash Kumar <Kailashbokaro123@gmail.com>
2024-07-30 00:40:58 +0530

    index page ready

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,31 @@
+// index page js
+document.getElementById('indexForm').addEventListener('submit',function(event){
+    event.preventDefault(); // Prevent the form from submitting
+    const name = document.getElementById("indexFormName");
+    const phone = document.getElementById("indexFormPhone");
+    const email = document.getElementById("indexFormEmail");
+    const msg = document.getElementById("indexFormMsg");
+    const errorMessage = document.getElementById('error-message');
+     // Regular expression to match exactly 10 digits
+     const mobileRegex = /^\d{10}$/;
+    try{
+     if (mobileRegex.test(phone.value)) {
+         errorMessage.textContent = 'Mobile number is valid.';
+         errorMessage.style.color = 'green';
+         alert("Message sent successfully....");
+     } else {
+         errorMessage.textContent = 'Please enter a valid 10-digit mobile number.';
+         errorMessage.style.color = 'red';
+         throw "Error";
+         
+     }
+    }
+     catch(error){
+        alert('Invalid request.....');
+     }
+     finally
+     {
+        console.log(name.value + "\n" + phone.value + "\n" + email.value + "\n" + msg.value);
+     }
+
+});
